5cb081dc94798dc1857ea4721ae314bff51e4fdc
Cadence Cooper <Ccooper4@mocs.flsouthern.edu>
2025-09-17 18:36:02 -0400

    Add advanced mathematical operations

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 def add(a, b):
     return a + b
 
@@ -8,4 +10,16 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
     return a / b
+
+def power(a, b):
+    return a ** b
+
+def square_root(a):
+    if a < 0:
+        return "Error: Cannor take square root of a negative number"
+    return math.sqrt(a)
+        
+
